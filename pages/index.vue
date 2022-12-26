f54c90c18f29629ceb2f3330cb77860b6062136a
Dmytro <dimores95@gmail.com>
2022-12-26 14:39:56 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Nuxt 2 template</h1>
+    <span class="test"> Hello there </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
+
+<style lang="sass" scoped>
+div
+  .test
+    color: red
+</style>
