224b96a55c46a8e45ee02c8faf09a66cd2407baa
Dmytro <dimores95@gmail.com>
2022-12-30 10:54:35 +0200

    corrected product card styles

--- components/CardProduct.vue
+++ components/CardProduct.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="card" :class="cardClass" @click="openCard">
-    <h3 class="card__title">{{ title }}</h3>
-    <img :src="imgSrc" :alt="category" class="card__img" />
-    <p class="card__desc">{{ description }}</p>
-    <p class="card__price">Preice: {{ price }}</p>
+    <h2 class="card__title">{{ title }}</h2>
+    <img :src="imgSrc" :alt="category" class="card__image" />
+
+    <div class="card__desc">
+      <h3 class="card__desc_title">Description:</h3>
+      <p>
+        {{ description }}
+      </p>
+    </div>
+    <div class="card__price">Preice: {{ price }}</div>
   </div>
 </template>
 
@@ -48,8 +54,16 @@ export default {
 
 <style lang="scss" scoped>
 .card {
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  padding: 10px 12px 14px 12px;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+  transition: all 0.3s ease;
+
   &:hover {
     cursor: pointer;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
   }
 
   &__title {
@@ -58,23 +72,47 @@ export default {
     letter-spacing: 0.3px;
     font-weight: normal;
     text-align: center;
+    font-size: 20px;
+    min-height: 54px;
   }
 
-  &__img {
-    width: 100%;
-    height: auto;
+  &__image {
+    display: block;
+    max-width: 100%;
+    height: 200px;
+    margin: 0 auto;
   }
 
   &__desc {
     color: #000;
     font-size: 14px;
     line-height: 1.8;
+    position: relative;
+    padding-bottom: 20px;
+
+    &_title {
+      text-transform: uppercase;
+      line-height: 26px;
+      text-transform: uppercase;
+      color: rgba(35, 31, 32, 0.5);
+      transition: 0.3s;
+    }
+
+    p {
+      overflow: hidden;
+      height: 20px;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+    }
   }
 
   &__price {
     font-size: 25px;
     line-height: 25px;
     color: #646464;
+    margin-top: auto;
+    padding-top: 16px;
   }
 }
 </style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,18 +1,23 @@
 <template>
   <section class="container">
-    <label for="category">
-      Category:
-      <select v-model="selectedCategory" id="category">
-        <option v-for="category in availableCategories" :value="category" :key="category">
-          {{ category }}
-        </option>
-      </select>
-    </label>
+    <div class="filters">
+      <label for="category">
+        Category:
+        <select v-model="selectedCategory" id="category" class="product-filter">
+          <option
+            v-for="category in availableCategories"
+            :value="category"
+            :key="category"
+          >
+            {{ category }}
+          </option>
+        </select>
+      </label>
+    </div>
 
-    <div class="filters"></div>
     <div class="row">
       <product-card
-        v-for="prod in productsAll"
+        v-for="prod in cards"
         :key="prod.id"
         :title="prod.title"
         :imgSrc="prod.image"
@@ -31,13 +36,12 @@
 import ProductCard from "@/components/CardProduct.vue";
 export default {
   async fetch({ store }) {
-    if (!store.getters.products) {
-      await store.dispatch("products/all");
-    }
-
-    if(!store.getters["category/category"]) {
-      await store.dispatch("category/AllCategories");
-    }
+    // if (!store.getters.products) {
+    //   await store.dispatch("products/all");
+    // }
+    // if (!store.getters["category/category"]) {
+    //   await store.dispatch("category/AllCategories");
+    // }
   },
 
   components: {
@@ -117,6 +121,24 @@ export default {
 .col {
   width: calc(100% / 3 - 30px);
   margin: 15px;
+}
+
+.filters {
+  margin-top: 30px;
+  margin-bottom: 26px;
+}
+
+.product-filter {
+  border: 1px solid #ccc;
+  border-radius: 0px;
+  display: inline-block;
+  width: 300px;
+  height: 34px;
+  padding: 6px 12px;
+  font-size: 14px;
+  line-height: 1.5;
+  color: #555;
+  background-color: #fff;
 }
 
 @media screen and (max-width: 920px) {
